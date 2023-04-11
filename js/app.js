a9cd587747056e5e8c3e0e36688a5b3970d5684b
David Adame Soto <david.adame.itd@gmail.com>
2023-04-11 11:39:35 -0600

    Funcionalidad y animacion agregada

--- js/app.js
+++ js/app.js
@@ -1,26 +1,65 @@
+// Globales
 const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
 
-const quote = document.querySelector('.info__quote');
+const quoteTxt = document.querySelector('.info__quote');
+const nameTxt = document.querySelector('#name');
+const rolTxt = document.querySelector('#rol');
+const imageUser = document.querySelector('#image-user');
+
+const spinner = document.querySelector('.spinner');
+const backdrop = document.querySelector('.backdrop');
 
 let index = 0;
-
-const consultarData = async () => {
-    const url = "https:/localhost:5500/data.json";
-    const respuesta = await fetch(url);
-    const resultado = await respuesta.json();
-    console.log(resultado);
-}
+let resultado;
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarData();
 });
 
+const consultarData = async () => {
+    spinner.style.display = "block";
+    backdrop.style.display = "block";
+    const url = "https://my-json-server.typicode.com/DavidXploidz/Testimonials_Slider/users";
+    const respuesta = await fetch(url);
+    resultado = await respuesta.json();
+    mostrarInfo(resultado[index]);
+    spinner.style.display = "none";
+    backdrop.style.display = "none";
+}
+
+function mostrarInfo(datos){
+    const { id, name, quote, rol, image } = datos;
+
+    quoteTxt.textContent = quote;
+    nameTxt.textContent = name;
+    rolTxt.textContent = rol;
+    imageUser.src = image;
+}
 
 prev.addEventListener('click', () => {
-    console.log('Prev');
+    index --;
+    animacion();
+    if(index < 0){
+        index = resultado.length - 1;
+    }
+    consultarData();
 });
 
 next.addEventListener('click', () => {
-    console.log('Next');
+    index ++;
+    animacion();
+    if(index > resultado.length - 1){
+        index = 0;
+    }
+    consultarData();
 });
+
+function animacion(){
+    quoteTxt.classList.add('info__quote--animate');
+    imageUser.classList.add('carrusel__user--animate');
+    setTimeout(() => {
+        quoteTxt.classList.remove('info__quote--animate');
+        imageUser.classList.remove('carrusel__user--animate');
+    },500)
+}
